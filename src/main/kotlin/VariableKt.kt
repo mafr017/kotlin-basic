707c3a1ba8c12d61e436b0ccc6298a4bd1f0bcb9
M Aditya Fathur Rahman <madityafr3@gmail.com>
2020-12-03 13:02:06 +0700

    Menambahkan file VariableKt.kt

---
+++ src/main/kotlin/VariableKt.kt
@@ -0,0 +1,11 @@
+fun main() {
+    //var = mutable
+    //val = immutable (rekomendasi untuk ketetapan), val tidak perlu mendeklarasikan tipe data
+
+    val phii = 3.14
+    val jarijari = 4
+
+    println(phii * jarijari * jarijari)
+
+
+}
