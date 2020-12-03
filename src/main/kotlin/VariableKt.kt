b7c953690f019916b107a9ce5d54ce4c6b2ac766
M Aditya Fathur Rahman <madityafr3@gmail.com>
2020-12-03 13:43:08 +0700

    menambah materi variable constant di file VariableKt.kt

--- src/main/kotlin/VariableKt.kt
+++ src/main/kotlin/VariableKt.kt
@@ -1,6 +1,11 @@
+const val APP = "Kotlin"
+const val VERSI = "v1.4.20"
+
 fun main() {
     //var = mutable
     //val = immutable (rekomendasi untuk ketetapan), val tidak perlu mendeklarasikan tipe data
+    //variable constant = untuk variable yang immutable (gunakan UPPERCASE untuk penamaan)
+    println("$APP: $VERSI\n")
 
     val phii = 3.14
     val jarijari = 4
