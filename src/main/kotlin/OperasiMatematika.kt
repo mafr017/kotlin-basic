d1d56f1c319756df3b93d53fb0c8141575ecdde1
M Aditya Fathur Rahman <madityafr3@gmail.com>
2020-12-03 19:52:22 +0700

    menambahkan materi augmented assignments dan unary operator di OperasiMatematika.kt

--- src/main/kotlin/OperasiMatematika.kt
+++ src/main/kotlin/OperasiMatematika.kt
@@ -8,4 +8,33 @@ fun main() {
     println("result 10/3 in Double = $result2")
     println("result 10/3 in Int versi val= $result3")
     println("result 10/3 in Double versi val = $result4")
+
+    //augmented assignments
+    // a += 10  ;  a -= 10  ; a *= 10  ; a /= 10  ; a %= 10
+
+    var total: Int = 0
+    val barang1 = 10
+    val barang2 = 14
+    val barang3 = 40
+
+    total += barang1
+    println(total)
+
+    total -= barang2
+    println(total)
+
+    total += barang3
+    println(total)
+
+    //unary operator
+    // ++ -> increment  ; -- -> decrement  ; +a -> positif  ; -a -> minus  ;  ! -> boolean
+    total++
+    total++
+    total++
+    total++
+    println(total)
+    println(-total)
+
+    val benar = true
+    println(!benar)
 }
