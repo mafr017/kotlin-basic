1a0649b8810d869bc664e38b2cd78193e220328d
M Aditya Fathur Rahman <madityafr3@gmail.com>
2020-12-03 19:43:20 +0700

    menambahkan file OperasiMatematika.kt

---
+++ src/main/kotlin/OperasiMatematika.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var result1: Int = 10 / 3
+    var result2: Double = 10.0 / 3.0
+    val result3 = 10 / 3
+    val result4 = 10.0 / 1.9
+
+    println("result 10/3 in Int = $result1")
+    println("result 10/3 in Double = $result2")
+    println("result 10/3 in Int versi val= $result3")
+    println("result 10/3 in Double versi val = $result4")
+}
