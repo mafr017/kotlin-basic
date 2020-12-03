d66abe6a9459d32c2107322d0684bc0539035beb
M Aditya Fathur Rahman <madityafr3@gmail.com>
2020-12-03 13:27:29 +0700

    mengubah file VariableKt.kt

--- src/main/kotlin/VariableKt.kt
+++ src/main/kotlin/VariableKt.kt
@@ -4,8 +4,14 @@ fun main() {
 
     val phii = 3.14
     val jarijari = 4
+    var warna : String? = null
+    warna = "Oranye"
 
+    print("Luas lingkaran: ")
     println(phii * jarijari * jarijari)
+    print("Warna lingkaran: ")
+    println(warna)
+    println(warna?.length)
 
 
 }
